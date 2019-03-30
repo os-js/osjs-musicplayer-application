effaadd0e06e922af64474d8052f76a95dfb4dfc
Anders Evenrud <andersevenrud@gmail.com>
2019-03-30 01:13:49 +0100

    Fixed metadata fetching

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const mm = require('music-metadata');
+const fs = require('fs');
 
 module.exports = (core, proc) => ({
   start: () => {},
   destroy: () => {},
   init: async () => {
     core.app.get(proc.resource('/metadata'), (req, res) => {
-      core.make('osjs/vfs').readfile(req.query, {}, req.session)
-        .then(stream => {
+      core.make('osjs/vfs').realpath(req.query.path, req.session.user)
+        .then(filename => {
+          const stream = fs.createReadStream(filename);
+
           return mm.parseStream(stream, undefined, {skipCovers: true})
             .then(metadata => res.json({error: null, metadata}))
             .catch(error => res.json({error}));
